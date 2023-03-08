99e0c59fc37e2096d64e05504b0426c39ab4b9f9
Karl Jääts <karl.jaats@gmail.com>
2023-03-08 11:43:28 +0200

    enable buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CXXFLAGS='-std=c++11' make target
+#CXXFLAGS='-std=c++11' make target
 
-sudo apt-get update
-sudo apt-get install autoconf automake libtool curl make g++ unzip
+#sudo apt-get update
+#sudo apt-get install autoconf automake libtool curl make g++ unzip
 
 ./autogen.sh
 
-./configure
-make CXXFLAGS='-std=c++14'
-sudo make install
-sudo ldconfig # refresh shared library cache.
+./configure && make
+#make CXXFLAGS='-std=c++14'
+#sudo make install
+#sudo ldconfig # refresh shared library cache.
