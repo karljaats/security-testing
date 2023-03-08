612a86720c7235c5d3af28c935ec52046b562ea1
Karl Jääts <karl.jaats@gmail.com>
2023-03-08 12:09:31 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -3,12 +3,12 @@
 CXXFLAGS='-std=c++11' make target
 
 sudo apt-get update
-sudo apt-get install autoconf automake libtool curl make g++ unzip
+sudo apt-get install autoconf automake libtool curl make g++ unzip libssl-dev
 
 ./autogen.sh
 
 ./configure
-make
-#make CXXFLAGS='-std=c++14'
+#make
+make CXXFLAGS='-std=c++14'
 sudo make install
 #sudo ldconfig # refresh shared library cache.
