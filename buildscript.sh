196b9f81ce6092515256a0d617c328245db54390
Karl Jääts <karl.jaats@gmail.com>
2023-03-08 12:56:24 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -3,7 +3,8 @@
 CXXFLAGS='-std=c++11' make target
 
 sudo apt-get update
-sudo apt-get install openssl=1.1.1f-1ubuntu2.17
+sudo apt-cache madison openssl
+sudo apt-get install -y --allow-downgrades openssl=1.1.1f-1ubuntu2.17
 
 openssl version
 apt list openssl
