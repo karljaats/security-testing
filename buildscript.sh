8526343960069d16b24954753ded2117e539c2f8
Karl Jääts <karl.jaats@gmail.com>
2023-03-09 10:14:31 +0200

    try openssl 1.1.0i

--- buildscript.sh
+++ buildscript.sh
@@ -3,10 +3,7 @@
 CXXFLAGS='-std=c++11' make target
 
 sudo apt-get update
-openssl_ver=`sudo apt-cache madison openssl | grep xenial-updates | awk '{print $3}'`
-libssl_ver=`sudo apt-cache madison libssl-dev | grep xenial-updates | awk '{print $3}'`
-[ -n "${openssl_ver}" ] && [ -n "${libssl_ver}" ] && \
-sudo apt-get install -y --allow-downgrades openssl=${openssl_ver} libssl-dev=${libssl_ver}
+sudo apt-get install -y --allow-downgrades openssl=1.1.0i
 
 openssl version
 apt list openssl
