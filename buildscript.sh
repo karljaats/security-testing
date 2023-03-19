b4402544f08a97ffa6406d797ab8be9a9daa5a19
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 12:49:06 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -8,11 +8,13 @@ sudo apt-get install protobuf-compiler python-protobuf
 pip install --upgrade protobuf grpcio-tools
 
 #bazel build
+chmod +rwx examples
+chmod +rwx examples/simple
+chmod +rwx examples/simple/Makefile
+chmod +rwx generator
+chmod +rwx generator/protoc
 
 cd ./examples/simple
-
-../../generator-bin/protoc --nanopb_out=. simple.proto
-protoc --plugin=protoc-gen-nanopb=nanopb/generator/protoc-gen-nanopb --nanopb_out=. myprotocol.proto
 make
 
 #cd ./examples/cmake_simple
