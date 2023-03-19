00cdd1ce5b99cb14b2894b1bd3167fe70925a66c
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 12:11:03 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -5,5 +5,9 @@ CXXFLAGS='-std=c++11' make target
 sudo apt-get update
 sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config doxygen scons git bazel
 
-bazel build
+#bazel build
 
+cd ./examples/simple
+ls
+make
+make install
