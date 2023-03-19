f0795a6aa158faeec67f918bb706e1d697500c81
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 11:46:57 +0200

    no cython

--- buildscript.sh
+++ buildscript.sh
@@ -12,7 +12,7 @@ python3 --version
 sudo apt-get update
 sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config doxygen
 
-./autogen.sh
+./autogen.sh --without-cython
 make CXXFLAGS='-std=c++14'
 sudo make install
 sudo ldconfig # refresh shared library cache.
