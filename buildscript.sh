1a719df910dd04697c322c4e6dae21d183d4b692
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 12:37:05 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -5,10 +5,13 @@ CXXFLAGS='-std=c++11' make target
 sudo apt-get update
 sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config doxygen scons git bazel
 sudo apt-get install protobuf-compiler python-protobuf
+pip install --upgrade protobuf grpcio-tools
 
 #bazel build
 
 cd ./examples/simple
+
+../../generator-bin/protoc --nanopb_out=. simple.proto
 protoc --plugin=protoc-gen-nanopb=nanopb/generator/protoc-gen-nanopb --nanopb_out=. myprotocol.proto
 make
 
