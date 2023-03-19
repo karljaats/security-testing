1058ff8d2a37c2d0de2fda88038b40af5eb8bd93
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 13:02:24 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -8,11 +8,15 @@ sudo apt-get install protobuf-compiler python-protobuf
 sudo apt-get install libprotoc-dev
 pip install --upgrade protobuf grpcio-tools
 
-#PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
-#curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
-#sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-#sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
-#rm -f $PROTOC_ZIP
+protoc --version
+
+PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+rm -f $PROTOC_ZIP
+
+protoc --version
 
 #bazel build
 chmod +rwx examples
