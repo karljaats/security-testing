66286d87fa8a0bd41c68a8b0b752bd234e3196ac
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 10:51:25 +0200

    python2

--- buildscript.sh
+++ buildscript.sh
@@ -3,7 +3,7 @@
 CXXFLAGS='-std=c++11' make target
 
 sudo apt-get update
-sudo apt-get install python
+sudo apt-get install python2
 sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config doxygen
 
 ./autogen.sh
