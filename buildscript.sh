7933e64b740c0dec9ed6b1823b89cc75b72fac93
Karl Jääts <karl.jaats@gmail.com>
2023-03-19 10:49:19 +0200

    add python install

--- buildscript.sh
+++ buildscript.sh
@@ -3,7 +3,8 @@
 CXXFLAGS='-std=c++11' make target
 
 sudo apt-get update
-sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config cython doxygen
+sudo apt-get install python
+sudo apt-get install make autoheader autoconf automake libtool curl make g++ gcc unzip pkg-config doxygen
 
 ./autogen.sh
 make CXXFLAGS='-std=c++14'
