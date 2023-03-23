9c0eeedcb8707ba782ed4d4129566cb0b09acd46
Karl Jääts <karl.jaats@gmail.com>
2023-03-23 12:43:11 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install autoconf automake libtool curl make g++ unzip libc-ares-dev libwebsockets-dev libssl-dev xsltproc docbook-xsl uthash utlist cJSON
+sudo apt-get install autoconf automake libtool curl make g++ unzip libc-ares-dev libwebsockets-dev libssl-dev xsltproc docbook-xsl uthash-dev libcjson-dev
 
 make
