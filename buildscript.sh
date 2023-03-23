dc7edf5b83242f5a4d46905a7f5222d8598d84fa
Karl Jääts <karl.jaats@gmail.com>
2023-03-23 10:03:50 +0200

    disable openssl

--- buildscript.sh
+++ buildscript.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-CXXFLAGS='-std=c++11' make target
-
-sudo apt-get update
-sudo apt-get install -y --allow-downgrades openssl=1.1.0i
-
-openssl version
-apt list openssl
-
 ./autogen.sh
+./configure --disable-openssl
 
-./configure
-#make
-make CXXFLAGS='-std=c++11'
-sudo make install
-#sudo ldconfig # refresh shared library cache.
+make
