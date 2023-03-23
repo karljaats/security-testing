202d8091c16eb7bdc315be6102e65ec408bafc4a
Karl Jääts <karl.jaats@gmail.com>
2023-03-23 14:15:38 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -21,6 +21,18 @@ sudo apt-get install \
 	libunwind8-dev \
     libelf-dev \
     libdwarf-dev
+	
+sudo ./build/fbcode_builder/getdeps.py install-system-deps --recursive
+	
+wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz && \
+tar zxf release-1.8.0.tar.gz && \
+rm -f release-1.8.0.tar.gz && \
+cd googletest-release-1.8.0 && \
+cmake . && \
+make && \
+make install
+
+cd ..
 
 cmake .
 make -j $(nproc)
