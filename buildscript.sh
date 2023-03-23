77f8a40a5517c11eb1416df26def4d2c7fb3f5f8
Karl Jääts <karl.jaats@gmail.com>
2023-03-23 12:57:23 +0200

    buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-CXXFLAGS='-std=c++11' make target
-
 sudo apt-get update
-sudo apt-get install autoconf automake libtool curl make g++ unzip
+sudo apt-get install autoconf automake libtool curl make g++ unzip libavcodec-dev libavutil-dev libavformat-dev \
+        libswresample-dev libavresample-dev \
+        libsamplerate-dev libsndfile-dev \
+        txt2man doxygen
 
-./autogen.sh
-
-./configure
-make CXXFLAGS='-std=c++14'
-sudo make install
-sudo ldconfig # refresh shared library cache.
+make
